224a9c42fac84cf6ac16b0829168b74b347d8998
Akshay Sharma <cto@pixelsimple.com>
2012-12-23 22:09:46 -0500

    changes to  play 2.1 upgrade

--- project/Build.scala
+++ project/Build.scala
@@ -1,20 +1,24 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "servercapture"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "servercapture"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-      "commons-codec" % "commons-codec" % "1.7",
-      "commons-io" % "commons-io" % "2.4"
-    )
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    jdbc,
+    anorm,
+    "commons-codec" % "commons-codec" % "1.7",
+    "commons-io" % "commons-io" % "2.4"
+    
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
 
 }
